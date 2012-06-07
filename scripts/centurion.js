a58f24bca1af8f5cb06c77510f3cca11e6deff34
Justin Hough <justinrayhough@gmail.com>
2012-06-06 22:19:31 -0700

    Updated Centurion to work with 3 less CSS files and added adapt.js for stylesheet swtiching

--- scripts/centurion.js
+++ scripts/centurion.js
@@ -6,30 +6,37 @@ $(document).ready(function(){
       $("tbody tr:odd", this).addClass("even");
     });
 	
-    // More scripting coming soon
-    
-		
-		// Navigation
-		var cUrlPath = jQuery.url.attr("path");
-		var cCurrentfile = jQuery.url.attr("file");
-		//var cDir = jQuery.url.attr("directory");
-		
-		if(cCurrentfile == null){
-			cUrlPath += "index.html";
+	// Navigation
+	var cUrlPath = jQuery.url.attr("path");
+	var cCurrentfile = jQuery.url.attr("file");
+	//var cDir = jQuery.url.attr("directory");
+
+	$("nav ul li a").each(function () {
+		var cNav = $(this).attr("href");
+		if(cNav.search(cCurrentfile) > -1) {
+			$(this).addClass("on");
 		}
-		
-		$("nav ul li a").each(function () {
-			var cNav = $(this).attr("href");
-			if(cNav.search(cCurrentfile) > -1) {
-				$(this).addClass("on");
-			}
-			if($("nav ul li a.on").size() > 1){
-				$("nav ul li a").removeClass("on");
-				//$("nav ul li a:first").addClass("on");
-			}
+		if($("nav ul li a.on").size() > 1){
+			$("nav ul li a").removeClass("on");
+		}
 			
-		});
-		
+	});	
 });
 
 
+// ADAPT.JS
+// Handles the stylesheet switching involved with mobile detection
+function centurionID(i, width) {
+  document.documentElement.id = 'centurion_' + i;
+}
+var ADAPT_CONFIG = {
+  dynamic: true,
+  callback: centurionID,
+  range: [
+    '0 to 320',
+    '320 to 480',
+    '480 to 700',
+    '700 to 1000',
+    '1000',
+  ]
+};

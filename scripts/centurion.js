41aa8553d6497366589793044857dc4e6400c77d
Justin Hough <justinrayhough@gmail.com>
2012-06-06 19:49:18 -0700

    switched stylesheets to SCSS versions

--- scripts/centurion.js
+++ scripts/centurion.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+
 	// Table Striping
 	$("table").each(function(){
       $("tbody tr:even", this).addClass("odd");
@@ -8,6 +8,28 @@ $(document).ready(function(){
 	
     // More scripting coming soon
     
+		
+		// Navigation
+		var cUrlPath = jQuery.url.attr("path");
+		var cCurrentfile = jQuery.url.attr("file");
+		//var cDir = jQuery.url.attr("directory");
+		
+		if(cCurrentfile == null){
+			cUrlPath += "index.html";
+		}
+		
+		$("nav ul li a").each(function () {
+			var cNav = $(this).attr("href");
+			if(cNav.search(cCurrentfile) > -1) {
+				$(this).addClass("on");
+			}
+			if($("nav ul li a.on").size() > 1){
+				$("nav ul li a").removeClass("on");
+				//$("nav ul li a:first").addClass("on");
+			}
+			
+		});
+		
 });
 
 

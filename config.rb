db6f62f3d6e2185d1fee21787f1a9a0d46ed3ef1
Justin Hough <justin@justinhough.com>
2014-01-08 13:33:07 -0800

    created new folder “assets” to help restructure files for CSS build and Sass file locations (hopefully to streamline 3.4)

--- config.rb
+++ config.rb
@@ -1,4 +1,8 @@
-css_dir = "css"
-line_comments = false # by Compass.app 
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+javascripts_dir = "assets/js"
+images_dir = "images"
 
-output_style = :expanded # by Compass.app 
+line_comments = false # set to true for debugging
+output_style = :expanded # no compression by default

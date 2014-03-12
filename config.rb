8aca71da4b2f5af2720cb9aec71a1d7249ab8cc1
Justin Hough <JHough@esri.com>
2014-03-11 23:17:34 -0700

    changing output style to remove code commenting

--- config.rb
+++ config.rb
@@ -5,4 +5,6 @@ javascripts_dir = "assets/js"
 images_dir = "images"
 
 line_comments = true # set to true for debugging
+
 output_style = :expanded # by Compass.app 
+line_comments = false # by Compass.app 

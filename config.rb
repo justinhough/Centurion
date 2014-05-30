2b06d219a2994edf71bcef803e20699830d12eeb
Justin Hough <JHough@esri.com>
2014-05-30 16:39:18 -0700

    gitignore updates and reconfigured base paths for Sass compile.

--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/sass"
-javascripts_dir = "assets/js"
-images_dir = "images"
+css_dir = "build/css"
+sass_dir = "src/assets/sass"
+javascripts_dir = "src/assets/js"
+images_dir = "src/assets/img"
 
 line_comments = true # set to true for debugging
 
 output_style = :expanded # by Compass.app 
-line_comments = false # by Compass.app 
+line_comments = true # by Compass.app 

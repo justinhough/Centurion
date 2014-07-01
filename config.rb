36de77894f50d7b9b9d229dedc6fbd0c916fb423
Justin Hough <JHough@esri.com>
2014-07-01 08:09:50 -0700

    Updating config.rb in case you just want to compile the sass

--- config.rb
+++ config.rb
@@ -1,10 +1,5 @@
-http_path = "/"
-css_dir = "public/css"
-sass_dir = "source/assets/sass"
-javascripts_dir = "source/assets/js"
-images_dir = "source/assets/img"
-
-line_comments = true # set to true for debugging
+sass_dir = "assets/sass"
+css_dir = "build/assets/css"
+line_comments = false # by Compass.app 
 
 output_style = :expanded # by Compass.app 
-line_comments = true # by Compass.app 

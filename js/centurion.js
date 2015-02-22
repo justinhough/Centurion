222f57b797a104d95e8479d3b496310f72399e5b
Justin Hough <justinrayhough@gmail.com>
2015-02-21 16:38:30 -0800

    Updates

--- js/centurion.js
+++ js/centurion.js
@@ -1,5 +1,24 @@
 // Centurion JS
 
+function centurionCurrent(elem) {
+  var mlen = 0,tsnm = null;
+  
+  //@todo: current navigation needs to account for "/" and find first nav item or index
+  
+  $(elem).find('a').each(function(){
+    var pat = $(this).attr('href');
+    
+    if(window.location.pathname.indexOf(pat) != -1 && pat.length > mlen){
+      mlen = pat.length
+      $(this).addClass('nav-current').siblings().removeClass('nav-current');
+    }
+  });
+}
+
+centurionCurrent('#main');
+
+//@todo: rewrite all functions into one centurion class based function.
+
 // Alerts
 function centurionAlerts() {
   $('.btn-expand').on('click',function(){
@@ -22,8 +41,6 @@ centurionAlerts();
 
 // Tables
 function centurionTables() {
-  
-
   $('.striped-table').each(function(){
 		//$('tbody tr:odd', this).addClass("odd");
 		$('tbody tr:even', this).addClass("even");
